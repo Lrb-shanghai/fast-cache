c20c777087a91f3ae4dd67a8f714ffc293065045
lrb <1275791923@qq.com>
2023-10-25 11:02:13 +0800

    0.0.0初步测试版本

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 刘荣兵
+ * @Date: 2023-10-25 10:48:16
+ * @LastEditors: 刘荣兵
+ * @LastEditTime: 2023-10-25 10:48:38
+ * @Description: 
+ * @FilePath: /前端缓存/fast-cache/src/index.js
+ */
+alert("fast-cache")
