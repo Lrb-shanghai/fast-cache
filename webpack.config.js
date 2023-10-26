be618de8e91ee0448a050b90df3b3a77fb8f8235
lrb <1275791923@qq.com>
2023-10-26 16:48:19 +0800

    删除一些文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname,
-        filename: './release/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-}
+/*
+ * @Author: 刘荣兵
+ * @Date: 2023-10-25 14:19:09
+ * @LastEditors: 刘荣兵
+ * @LastEditTime: 2023-10-26 16:48:05
+ * @Description: 
+ * @FilePath: /前端缓存/fast-cache/webpack.config.js
+ */
