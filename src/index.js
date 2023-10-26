4521583389bb9c27dc87d5dd6930368c964e259f
lrb <1275791923@qq.com>
2023-10-26 14:06:01 +0800

    修改文件内容

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * @Author: 刘荣兵
  * @Date: 2023-10-25 10:48:16
  * @LastEditors: 刘荣兵
- * @LastEditTime: 2023-10-25 10:48:38
+ * @LastEditTime: 2023-10-26 14:04:53
  * @Description: 
  * @FilePath: /前端缓存/fast-cache/src/index.js
  */
-alert("fast-cache")
+alert('fast-cache')
